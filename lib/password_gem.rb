ad9549dc7e99502e0bce77eaa9a33ff55bddd8e9
AldarBis <33025891+AldarBis@users.noreply.github.com>
2018-10-14 22:36:06 +0300

    Update password_gem.rb

--- lib/password_gem.rb
+++ lib/password_gem.rb
@@ -1,46 +1,45 @@
-module PasswordGem
-  
-  def self.password_fixed(len)
+
+class Password
+	
+  def generate(quant = 1, len)
+    unless quant.is_a?(Integer)
+      raise "Неверный тип данных! Количество паролей не может быть #{quant.class}"
+    end
+    unless len.is_a?(Integer) || len.is_a?(Range)
+      raise "Неверный тип данных! Длинна пароля не может быть #{len.class}"
+    end
     constants = "bcdfghjklmnpqrstvwxyz".scan(/.{1}/)
     vowels = "aouie".scan(/.{1}/)
     compound = constants | ["ng", "sh", "nk", "ch", "wr", "tch", "qu", "ck", "wh", "th"]
-    word = ""
     l = rand(1..9)
-
-    while (word.length < len) do 
-      if  (l % 2 ) == 0
-        word = word + vowels.sample
-      else
-        case word.length
-	      when len - 1, 0
-            compound = constants
-          when len - 2
-            compound = compound - ["tch"]
-        end		
-	    word = word + compound.sample
-	  end
-      l += 1
-    end
-    return word
-  end
-
-  def self.password_interval(interv)
-    password_fixed(interv.to_a.sample)
-  end
-
-  def self.password_quant(quant, len)
     passwords = []
-    if len.is_a?(Integer)
-      quant.times do
-	    passwords.push(password_fixed(len))
+    quant.times do
+      word = ""
+      if len.is_a?(Range)
+        len = len.to_a.sample
       end
-    end
-  
-    if len.is_a?(Range)
-      quant.times do
-	    passwords.push(password_interval(len))
+      while (word.length < len) do 
+        if (l % 2 ) == 0
+          word = word + vowels.sample
+        else
+          case word.length
+	    when len - 1, 0
+	      compound = constants
+	    when len - 2
+	      compound = compound - ["tch"]
+          end		
+          word = word + compound.sample
+        end
+        l += 1
       end
+      passwords.push(word)
     end
     return passwords
   end
+	
 end
+
+
+
+ 
+ 
