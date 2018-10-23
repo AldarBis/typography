4fed0892d79ae220cd53370965c7175e233a5a33
AldarBis <aldarbiyusov@gmail.com>
2018-10-23 19:31:43 +0000

    Finish typograf

--- lib/typog_gem.rb
+++ lib/typog_gem.rb
@@ -1,20 +1,32 @@
 
 require 'json'
 require 'net/http'
-require 'nokogiri'
 
-class Typograph
-  
-  def initialize(source)
-    @source = source
+  class Validator
+    
+    def validate(text)
+      unless text.is_a?(String)
+        raise TypeError.new("Argument could not be an #{text.class}")
+      end
+      if text.empty?
+        raise ArgumentError.new("Text could not be empty") 
+      end
+    end
+    
   end
   
-  def typo
-    uri = URI.parse('http://mdash.ru/api.v1.php?')
-    params = URI.decode_www_form(uri.query)
-    params << ['text', @source]
-    uri.query = URI.encode_www_form(params)
-    uri = URI(uri.to_s)
-    Nokogiri::HTML(JSON.parse(Net::HTTP.get(uri))["result"]).text
+  class Typograph < Validator
+   
+    def initialize(raw_text)
+      @raw_text = raw_text
+    end
+  
+    def typo
+      validate(@raw_text)
+      uri = URI('http://mdash.ru/api.v1.php')
+      res = Net::HTTP.post_form(uri, 'Host' => 'mdash.ru', 'Content-type' => 'multipart/form-data',
+      'Content-Length' => '24', 'text' => @raw_text)
+      JSON.parse(res.body)["result"]
+    end
+    
   end
-end
